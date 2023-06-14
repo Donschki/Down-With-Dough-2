61b659b488ebf56be49e0a97a5f318ab22976063
donschki <donvonkrog@gmail.com>
2023-06-14 16:26:16 +0200

    a

--- Js/Script.js
+++ Js/Script.js
@@ -5,7 +5,10 @@ const hamburger = document.querySelector(".hamburgerBox");
 hamburger.addEventListener('click', () => {
     sideBar.classList.toggle("change");
     body.click.toggle("change");
+
 });
+
+
 
 
 // SLIDESHOW SCRIPT 
@@ -27,18 +30,14 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
+  setTimeout(showSlides, 6000); // Change image every 4 seconds
 }
 
-// OFF CLICK SCRIPT 
+// CLICK TO REMOVE TOP-BAR MENU
 
- document.querySelector('main').addEventListener('click', () => {
- sideBar.classList.remove('change');
-});
 
- document.querySelector('.contact-info-container').addEventListener('click', () => {
- sideBar.classList.remove('change');
-});
+
+
 
 
 
